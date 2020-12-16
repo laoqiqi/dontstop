1ea12f85c53a1e5ad0c7ed2799fd009e6d280c85
laoqiqi <2313814787@qq.com>
2020-12-16 13:08:06 +0800

    tiaoshi

--- source/application/store/controller/Index.php
+++ source/application/store/controller/Index.php
@@ -15,18 +15,18 @@ class Index extends Controller
 
     public function index()
     {
-        $object = new \app\zhuce\TestTree();
-
-        //注册一个树位置
-        RegistrationTree::set('qiwen','data');
-        //获取值绑定到树上的果子
-        $data = RegistrationTree::get('qiwen')->getTreeContent();
-
-        $info = RegistrationTree::get('qiwen');
-
-        dump($info);die;
-//        $data = new User;
-//        return $this->fetch('index');
+//        $object = new \app\zhuce\TestTree();
+//
+//        //注册一个树位置
+//        RegistrationTree::set('qiwen','data');
+//        //获取值绑定到树上的果子
+//        $data = RegistrationTree::get('qiwen')->getTreeContent();
+//
+//        $info = RegistrationTree::get('qiwen');
+//
+//        dump($info);die;
+        $data = new User;
+        return $this->fetch('index');
     }
 
     public function data()
